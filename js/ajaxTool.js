958e604d4e1b03b029a0c6dd017ddf9d1f292ddb
chengpeng377248283 <>
2018-10-12 17:12:34 +0800

    pcmall

---
+++ js/ajaxTool.js
@@ -0,0 +1,128 @@
+/*封装通用ajax*/
+function publicAjax(url, type, data, callback) {
+    $.ajax({
+        url: url,
+        type: type,
+        data: data,
+        success: callback,
+    })
+}
+
+//带token的ajax
+function publicAjaxToken(url, type, data, callback, async) {
+    async = async || true;
+    var token = $.cookie('token');
+    $.ajax({
+        contentType: "application/x-www-form-urlencoded; charset=utf-8",
+        beforeSend: function (xhr) {
+            //发送ajax请求之前向http的head里面加入验证信息
+            xhr.setRequestHeader("token", token); // 请求发起前在头部附加token
+        },
+        url: url,
+        type: type,
+        data: data,
+        async: async,
+        success: callback,
+    })
+}
+
+//formdata传递的ajax
+function publicAjaxFormdata(url, type, data, callback) {
+    var token = $.cookie('token');
+    $.ajax({
+        beforeSend: function (xhr) {
+            //发送ajax请求之前向http的head里面加入验证信息
+            xhr.setRequestHeader("token", token); // 请求发起前在头部附加token
+        },
+        url: url,
+        type: type,
+        data: data,
+        processData: false,  // 不处理数据
+        contentType: false,  // 不设置内容类型
+        success: callback,
+    })
+}
+
+/*定义ajax的基本路径*/
+//海报的前缀
+// var posterUrl="http://nwx.tongyoutrip.com/";
+// var oneselfUrl="http://nwx.tongyoutrip.com/"
+// var posterUrl="http://2y0640281b.imwork.net:48474/";
+// var oneselfUrl="http://2y0640281b.imwork.net:48474/";
+var posterUrl="http://web.tongyoutrip.com/";
+var oneselfUrl="http://web.tongyoutrip.com/";
+/*正式服务器*/
+var baseAjaxUrl = "http://api.tongyoutrip.com";          //接口
+var baseImgUrl="http://image.tongyoutrip.com";           //图片服务器
+/*线上先下测试服务器*/
+// var baseAjaxUrl="http://192.168.0.16:8060";                 //小许
+// var baseAjaxUrl="http://192.168.0.148:8060";                 //小许
+// var baseAjaxUrl="http://2y0640281b.imwork.net:35024";    //本地测试
+// var baseImgUrl="http://192.168.0.148:80";
+// var baseImgUrl="http://192.168.0.16:9000";                  //小马
+
+// var baseAjaxUrl="http://192.168.0.192:8060";
+
+//获取token
+function getToken() {
+    // delLocal("token");
+    // alert(getLocal("token"));
+    var token = getLocal("token");
+    console.log("dsdffd" + token);
+    // alert(token)
+    //用于判断获取微信的code
+
+}
+
+// $.cookie('token', null);
+console.log($.cookie('token'));
+
+function getUser() {
+
+    /*获取用户的详细信息，并且存储起来*/
+    publicAjaxToken(baseAjaxUrl + "/ty_api/user/getDetail", "POST", {}, getUserInfoCall);
+
+    //用户信息的回调
+    function getUserInfoCall(res){
+        if(res.errorCode == 200){
+            var html = '<li><a href="userinfo.html"><p id="nickName">'+res.data.nickName+'</p></a></li>'+
+            '<li><p style="color: rgb(41, 75, 230);font-size:14px;padding:0 0 0 10px;" onclick="exit();"> 退出</p></li>';
+            $(".topLogin ul").html(html);
+        }
+    }
+}
+function getUserInfo() {
+
+    /*获取用户的详细信息，并且存储起来*/
+    publicAjaxToken(baseAjaxUrl + "/ty_api/user/getDetail", "POST", {}, getUserInfoCall);
+
+    //用户信息的回调
+    function getUserInfoCall(res){
+        if(res.errorCode == 200){
+            var html = '<li><a href="userinfo.html"><p id="nickName">'+res.data.nickName+'</p></a></li>'+
+            '<li><p style="color: rgb(41, 75, 230);font-size:14px;padding:0 0 0 10px;" onclick="myExit();"> 退出</p></li>';
+            $(".topLogin ul").html(html);
+        }
+    }
+}
+
+function exit(){
+    $.cookie('token', null);
+    window.location.reload()
+}
+
+function myExit(){
+    $.cookie('token', null);
+    location.href = "index.html";
+}
+
+/*sessionStorage*/
+//设置sessionStorage
+function setSess(name,val) {
+    sessionStorage.setItem(name,val) //存储名字为name值为caibin的变量
+}
+
+//获取sessionStorage
+function getSess(name) {
+    return sessionStorage.getItem(name);
+}
---
+++ js/public.js
@@ -0,0 +1,201 @@
+//公共获取地址栏信息
+function GetQueryString(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if (r != null) return unescape(r[2]);
+    return null;
+}
+
+// 轮播
+function lunbo() {
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        autoplay:true,
+        
+        // 如果需要分页器
+        pagination: {
+            el: '.swiper-pagination',
+            clickable :true,
+        },
+        navigation: {
+            nextEl: '.button-next',
+            prevEl: '.button-prev',
+        },
+    })
+}
+
+// 搜索框
+function searchSites(){
+    var searchtext = $("#searchtext").val();
+    if(searchtext != "" || searchtext != null){
+        location.href = "scenic.html?content="+searchtext;
+    } 
+}
+
+//转换时间
+function timeTransformation(time, type) {
+    // new Date('2014-02-18T15:00:48'.replace(/\s/, 'T'))
+    var datatime = new Date(time);
+    var year = datatime.getFullYear();//取得年
+    var month = datatime.getMonth() + 1;    //取得月,js从0开始取,所以+1
+    var date1 = datatime.getDate(); //取得天
+    var hour = datatime.getHours();//取得小时
+    var minutes = datatime.getMinutes();//取得分钟
+    var second = datatime.getSeconds();//取得秒
+    // alert(month);
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+    if (month < 10) {
+        month = "0" + month;
+    }
+    if (date1 < 10) {
+        date1 = "0" + date1;
+    }
+
+    if (type == "time") {
+        return hour + ':' + minutes;
+    } else if (type == "hour") {
+        return hour;
+    } else if (type == "minutes") {
+        return minutes;
+    } else if (type == "year") {
+        return year;
+    } else if (type == "month") {
+        return month;
+    } else if (type == "day") {
+        return date1;
+    } else if (type == "date") {
+        return month + '月' + date1 + '日';
+    } else if (type == "dateCount") {
+        return year + '-' + month + '-' + date1;
+    } else if (type == "timeCount") {
+        return hour + ':' + minutes + ':' + second;
+    } else {
+        return year + '-' + month + '-' + date1 + ' ' + hour + ':' + minutes + ':' + second;
+    }
+}
+//将中文日期替换成——
+function DayReg(day,packageYear) {
+    var firstPo = day.indexOf("月");
+    var lastPo = day.indexOf("日");
+    var m = day.substring(0, firstPo);
+    var d = day.substring(firstPo + 1, lastPo);
+    if (m.length == 1) {
+        m = "0" + m;
+    }
+    if (d.length == 1) {
+        d = "0" + d;
+    }
+    // var y = new Date().getFullYear();
+    return (packageYear + '-' + m + '-' + d);
+}
+
+//将——替换成中文日期
+function replaceSeparator(day, type) {
+    type = type || "MD";
+    var dayArr = day.split("-");
+    var result = '';
+    if (dayArr.length == 3) {
+        if (type == "YMD") {
+            result = dayArr[0] + "年" + dayArr[1] + "月" + dayArr[2] + "日";
+        } else {
+            result = dayArr[1] + "月" + dayArr[2] + "日";
+        }
+    }
+    return result;
+}
+
+//手机号码验证
+function mobileReg(phone) {
+    var reg = /^1[34578][0-9]{9}$/;
+    return reg.test(phone);
+}
+//验证身份证
+function IDcardReg(card) {
+// 身份证号码为15位或者18位，15位时全为数字，18位前17位为数字，最后一位是校验位，可能为数字或字符X
+    var reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
+    return reg.test(card);
+}
+//验证邮箱
+function emailReg(mail) {
+    // var szReg = /^[A-Za-zd]+([-_.][A-Za-zd]+)*@([A-Za-zd]+[-.])+[A-Za-zd]{2,5}$/;
+    var szReg = /^[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/;
+    return szReg.test(mail);
+}
+
+//封装统一弹框
+function layMessage(msg, type) {
+    type = type || 0;
+    layer.msg(msg,{
+        // content: msg
+        offset: '100px'
+        , time: 2000 //2秒后自动关闭
+    });
+}
+
+// 获取验证码
+var countSeconds=60;//验证码倒计时
+var aa;             //延迟变量
+$("#checkCode").click(function () {
+    var phone=$("#phone").val();
+    if(phone==''){
+        layMessage("请输入手机号！");
+        return false;
+    }
+    if(!mobileReg(phone)){
+        layMessage("手机格式不正确");
+        return false;
+    }
+
+    clearTimeout(aa);//清除延迟
+    settime();
+    $("#checkCode").attr("disabled",true)
+
+    var code=$.md5(phone+"APP_20!$_TY_HZ");
+    console.log(code);
+    //向手机发送验证码
+    publicAjax(baseAjaxUrl+"/ty_api/user/sendMessage","POST",{"code":code,"mobile":phone},sendInfoCall);
+
+});
+
+//验证码倒计时
+function settime() {
+    if(countSeconds==0){
+        $("#checkCode").attr("disabled",false)
+        $("#checkCode").html("获取验证码");
+        countSeconds=60;
+        return;
+    }else{
+        $("#checkCode").html("剩余"+countSeconds+"s");
+        countSeconds--;
+    }
+    aa=setTimeout(function () {
+        settime()
+    },1000)
+}
+//发送短信的回调
+function sendInfoCall(res) {
+    if(res.errorCode==200){
+        layMessage(res.message);
+    }else{
+        layMessage(res.message);
+    }
+}
+
+// 合作伙伴图片tab
+$(".invest-dot li").each(function (i, v) {
+    $(v).mouseover(function (){
+        $(this).addClass("change").siblings().removeClass("change");
+        $(".invest-list ul").eq(i).show();
+        $(".invest-list ul").eq(i).siblings().hide();
+        
+    });
+});
+    
